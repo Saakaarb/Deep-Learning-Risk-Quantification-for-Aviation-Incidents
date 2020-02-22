12fbd065f35aa986a40f5a53d6028dc4d13692f0
Saakaar Bhatnagar <40839313+Saakaarb@users.noreply.github.com>
2020-02-21 22:49:10 -0800

    Add files via upload

--- Data_prep/data_wrangle.py
+++ Data_prep/data_wrangle.py
@@ -1,26 +1,48 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[65]:
+# In[1]:
 
 
 import numpy as np
 import math
 import pandas as pd
+import re
 from copy import deepcopy
-
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize 
+import pickle
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+from textblob import TextBlob 
 def pause():
     programPause = raw_input("Press the <ENTER> key to continue...")
 
 def isNaN(num):
     return num != num    
-    
+
+def remove_stopwords(example_sent):
+  
+#example_sent = "This is a sample sentence, showing off the stop words filtration."
+  
+    stop_words = set(stopwords.words('english')) 
+
+    word_tokens = word_tokenize(example_sent) 
+
+    filtered_sentence = [w for w in word_tokens if not w in stop_words] 
+
+    filtered_sentence = [] 
+
+    for w in word_tokens: 
+        if w not in stop_words: 
+            filtered_sentence.append(w) 
+
+    return filtered_sentence
+
 data=pd.read_csv("Concatenate.csv")
-print(data.shape)
 
 #----------------------------------
 input_column_list=[9,10,11,12,16,17,19,22,24,37,40,63,66,70,83,85,87];
-input_default=["unknown","normal","normal","unkown","unknown","unknown",0,"unknown","unkown","unknown","unknown","unknown","unknown","unknown","unknown","no","unknown"];
+input_default=["unknown","normal","normal","unknown","unknown","unknown",0,"unknown","unkown","unknown","unknown","unknown","unknown","unknown","unknown","N","unknown"];
 
 output_column_list=[88,89];
 
@@ -81,7 +103,6 @@ for i_list in [0]: # Get indices to remove
         if isNaN(sublist[j_list]):
             
             index_toremove.append(j_list);
-print(np.array(index_toremove).shape)                        
 #Remove the records from all lists correspnding to index_toremove
 
 for i_remove in range(len(index_toremove)):
@@ -108,17 +129,45 @@ for i in range(len(input_data)):
         
         if isNaN(input_data[i][j]):
             raise Exception("NaN found");
-            
+
+#--------------
+
+print("Relevant Columns Selected")
+
+'''
+with open('input_data', 'wb') as f:
+    pickle.dump(input_data, f)
+    
+with open('output_data', 'wb') as f:
+    pickle.dump(output_data, f)
+with open('narrative_data', 'wb') as f:
+    pickle.dump(narrative_data, f)
+with open('synopsis_data', 'wb') as f:
+    pickle.dump(synopsis_data, f)
+
 
 
 save_input=pd.DataFrame(input_data);
 save_output=pd.DataFrame(output_data);
 save_narrative=pd.DataFrame(narrative_data);
 save_synopsis=pd.DataFrame(synopsis_data);
-print(save_input.shape)
-print(save_output.shape)
-print(save_narrative.shape)
-print(save_synopsis.shape)
+
+with open('save_input', 'wb') as f:
+    pickle.dump(save_input, f)
+    
+with open('save_output', 'wb') as f:
+    pickle.dump(save_output, f)
+with open('save_narrative', 'wb') as f:
+    pickle.dump(save_narrative, f)
+with open('save_synopsis', 'wb') as f:
+    pickle.dump(save_synopsis, f)
+'''
+#print(save_input.shape)
+#print(save_output.shape)
+#print(save_narrative.shape)
+#print(save_synopsis.shape)
+
+
     #if col=="Environment":
     #    print(col=="Environment")
     
@@ -128,6 +177,114 @@ print(save_synopsis.shape)
 #print(input_data[1][1])
     
 
+
+# In[2]:
+print("Starting Data Cleaning")
+
+num_datapoints=len(input_data[14][:])
+num_columns=len(input_data);
+input_default=["unknown","normal","normal","unknown","unknown","unknown",0,"unknown","unkown","unknown","unknown","unknown","unknown","unknown","unknown","N","unknown"];
+
+default_datatype=[type(input_default[i]) for i in range(len(input_default))];
+
+print("Cleaning Input Feature Data")
+
+for i_column in range(num_columns):
+    if i_column==6:                #Hard coded column number
+        continue;
+    for i_datapoint in range(num_datapoints):
+        #print(input_data[i_column][i_datapoint])
+        #if is_integer(input_data[i_column][i_datapoint]):
+         #   print("booyah")
+         #   input_data[i_column][i_datapoint]=input_default[i_column];
+         #   continue;
+        
+        string_1=input_data[i_column][i_datapoint];
+        string_1=string_1.lower()                     #Lowercase
+
+        string_1 = re.sub(r'[^\w\s]','',string_1)     # remove punctuation
+        input_data[i_column][i_datapoint]=string_1
+
+# Convert to Pd dataframe         
+input_data=pd.DataFrame(input_data);
+
+input_data=input_data.drop(input_data.index[[0,2,11,12,13]],axis=0)
+
+print("Done")
+print("SELECTING ONLY 5 RELEVANT COLUMNS")
+# For first iteration of the project, select 5 columns
+
+
+input_data.iloc[:,[30]]
+
+input_data=input_data.iloc[[0,2,4,6,9],:]
+
+
+# In[3]:
+print("Cleaning Narrative data")
+
+# Narrative data pre-processing
+narrative_data_main=deepcopy(narrative_data[0])
+for i_datapoint in range(num_datapoints):
+
+        
+    string_1=narrative_data_main[i_datapoint];
+    string_1=string_1.lower()                     #Lowercase
+
+    string_1 = re.sub(r'[^\w\s]','',string_1)     # remove punctuation
+    narrative_data_main[i_datapoint]=string_1
+    
+    narrative_data_main[i_datapoint]=TreebankWordDetokenizer().detokenize(remove_stopwords(narrative_data_main[i_datapoint])) #remove stopwords
+print("Done")
+print("Obtaining Narrative Sentiment Scores")
+narrative_scores=[];
+pos_weight=0.9;
+sub_weight=0.1;
+for i in range(len(narrative_data_main)):
+
+    pos_score=TextBlob(narrative_data_main[i]).sentiment[0]
+    sub_score=TextBlob(narrative_data_main[i]).sentiment[1]
+    narrative_scores.append(pos_weight*pos_score+sub_weight*sub_score)
+print("Done")
+
+print(len(narrative_scores))
+#--------------------------- One-hot encode risk-mapping
+with open('risk_mapping.pkl', 'rb') as f:
+    risk_data = pickle.load(f)
+
+encoded_risk=np.zeros([len(risk_data),5])
+for i in range(len(risk_data)):
+
+    for i_enc in range(5):
+
+        if int(i_enc+1)==risk_data[i]:
+            #print(i,i_enc)
+            encoded_risk[i,i_enc]=1
+            break;
+print(risk_data[0])
+print(encoded_risk[0])
+
+#--------------------------
+# Slice Dataset: Small input dataset for demo purposes
+
+slice_index=5000
+
+input_data=input_data[:slice_index,:]
+narrative_scores=narrative_scores[:slice_index]
+encoded_risk=encoded_risk[:slice_index,:]
+
+print(input_data.shape)
+#
+
+print("Save narrative and input dat using pickle")
+#save the required data
+with open('input_data_test', 'wb') as f:
+    pickle.dump(input_data, f)
+ 
+with open('narrative_data_test', 'wb') as f:
+    pickle.dump(narrative_scores, f)
+with open('risk_encoding', 'wb') as f:
+    pickle.dump(encoded_risk, f)
 
 # In[ ]:
 
